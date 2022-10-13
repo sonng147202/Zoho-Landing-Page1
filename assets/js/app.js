97aec780a4c05719ac4ca9eda2389b8c526ac8de
sonng147202 <sonnguyen20140720@gmail.com>
2022-10-13 17:47:15 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,3 @@
 $( document ).ready(function() {
-    var bgServices = [
-        "#FFBFBF",
-        "#BCF2BA",
-        "#C6DFFF",
-        "#FEE7B2",
-        "#818CF280",
-        "#B798EB80"
-    ]
-    $(".service .service__container .service__item").each((index) => {
-        $( this ).css("background-color", bgServices[index]);
-        console.log($( this ));
-    });
+    
 });
